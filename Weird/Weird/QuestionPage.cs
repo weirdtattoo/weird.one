e3bdede435f630085f2f87982ee6f5ec7667aab6
kazi.develop <kazi.develop@gmail.com>
2016-10-17 21:31:34 +0200

    UI.... working on the question page with the test data

--- Weird/Weird/QuestionPage.cs
+++ Weird/Weird/QuestionPage.cs
@@ -1,32 +1,219 @@
-﻿using Weird.Data;
-using Xamarin.Forms;
+﻿using System.Collections.Generic;
+using Weird.Data;
+using Weird.Services;
+
 
 namespace Weird
 {
-    public class QuestionPage : ContentPage
+    using System;
+    using Xamarin.Forms;
+
+
+    class QuestionPage : ContentPage
     {
-        private readonly IWeirdDatabase _database;
-        private readonly ListView _thoughtList;
+        
+        int clickTotal = 0;
+        private readonly QuestionSessionManager _testSm;
+        private readonly Question _question;
+    
+        private ContentView _contentView;
+        private RelativeLayout _relativeLayout;
 
         public QuestionPage(IWeirdDatabase database)
         {
-            _database = database;
-            Title = "Questions";
-            var thoughts = _database.GetRandomQuestions(10);
 
-            _thoughtList = new ListView();
-            _thoughtList.ItemsSource = thoughts;
-            _thoughtList.ItemTemplate = new DataTemplate(typeof(TextCell));
-            _thoughtList.ItemTemplate.SetBinding(TextCell.TextProperty, "Id");
-            _thoughtList.ItemTemplate.SetBinding(TextCell.DetailProperty, "Text");
+            this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            _testSm = new QuestionSessionManager(database);
+            _question = _testSm.GetNewQuestion();
 
 
-            Content = _thoughtList;
+            Label header = new Label
+            {
+                Text = _question.Text,
+                Font = Font.BoldSystemFontOfSize(50),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+
+          
+
+            _relativeLayout = new RelativeLayout();
+
+            _relativeLayout.Children.Add(header, Constraint.RelativeToParent((parent) =>
+            {
+                return 0;
+            }));
+
+           
+            _contentView  = new ContentView();
+
+
+            var stackLayout = new StackLayout();
+
+ 
+            AddButtons(stackLayout, _question.Answers);
+
+          
+
+            stackLayout.Spacing = 10;
+
+
+
+
+            Button button = new Button
+            {
+                  
+                     Text = "Check",
+                     Font = Font.SystemFontOfSize(NamedSize.Large),
+                     BorderWidth = 1,
+                     HorizontalOptions = LayoutOptions.Center,
+                     VerticalOptions = LayoutOptions.CenterAndExpand
+                 };
+            button.Clicked += OnButtonClicked;
+
+            _relativeLayout.Children.Add(button, Constraint.RelativeToParent((parent) => {
+                return 0;
+            }));
+
+            _relativeLayout.Children.Add(stackLayout, Constraint.RelativeToView(header, (parent, sibling) => {
+                return sibling.Height + 120;
+            }));
+
+            //https://forums.xamarin.com/discussion/17742/relativelayout-in-xaml
+
+
+
+
+
+            this.Content = _relativeLayout;
+
+
+ 
         }
 
-        public void Refresh()
+        private static void AddButtons(StackLayout stackLayout, List<Answer> questionAnswers)
         {
-            _thoughtList.ItemsSource = _database.GetRandomQuestions(10);
+            foreach (var questionAnswer in questionAnswers)
+            {
+                var button2 = new Button
+                {
+                    Text = questionAnswer.Text,
+                    AutomationId = questionAnswer.Id.ToString()
+
+                };
+                
+
+                stackLayout.Children.Add(button2);
+            }
+         
+
+        }
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            var questionAnswer = _testSm.CheckAnswer(_question.Id, 2);
+            _contentView.Content = new Label {Text = questionAnswer.Explanation};
+
+            var parentContainer = (RelativeLayout) Content;
+          
+
+            parentContainer.Children.Add(_contentView,
+              Constraint.RelativeToParent((parent) => {
+                  return parent.Width / 3;
+              }),
+              Constraint.RelativeToParent((parent) => {
+                  return parent.Height / 2;
+              }));
+
+
+
         }
     }
 }
+
+
+//    public class QuestionPage : ContentPage
+//    {
+//        private readonly IWeirdDatabase _database;
+//        private readonly ListView _thoughtList;
+
+//        public QuestionPage(IWeirdDatabase database)
+//        {
+//            _database = database;
+//            Title = "Questions";
+//            var thoughts = _database.GetRandomQuestions(10);
+
+
+//            var testSm = new QuestionSessionManager(new TestWeirdDatabase());
+//            var question = testSm.GetNewQuestion();
+
+
+//            //    for (int i = 0; i < 10; i++)
+//            //    {
+//            //        Assert.IsTrue(i < 5);
+//            //        var question = testSm.GetNewQuestion();
+
+//            //        if (i == 0)
+//            //        {
+//            //            var questionAnswer = testSm.CheckAnswer(question.Id, 2);
+//            //            Assert.AreEqual(questionAnswer.IsCorrect, false);
+//            //            Assert.AreEqual(questionAnswer.Explanation, "One plus one is two");
+
+//            //        }
+
+
+//            //        if (i == 1)
+//            //        {
+//            //            var questionAnswer = testSm.CheckAnswer(question.Id, 2);
+//            //            Assert.AreEqual(questionAnswer.IsCorrect, false);
+//            //            Assert.AreEqual(questionAnswer.Explanation, "(often be absorbed in) take up the attention of(someone); interest greatly");
+
+//            //        }
+
+//            //        if (i == 2)
+//            //        {
+//            //            var questionAnswer = testSm.CheckAnswer(question.Id, 2);
+//            //            Assert.AreEqual(questionAnswer.IsCorrect, true);
+//            //            Assert.AreEqual(questionAnswer.Explanation, "Accuse of");
+
+//            //        }
+
+
+//            //        if (i == 3)
+//            //        {
+//            //            var questionAnswer = testSm.CheckAnswer(question.Id, 2);
+//            //            Assert.AreEqual(questionAnswer.IsCorrect, true);
+//            //            Assert.AreEqual(questionAnswer.Explanation, "“It’s” is only ever used when short for “it is”. “Its” indicates something belonging to something that isn’t masculine or feminine (like “his” and “hers”, but used when you’re not talking about a person).");
+
+//            //        }
+
+
+//            //        if (question.IsLastQuestion)
+//            //        {
+//            //            var questionSummary = testSm.GetSummary();
+//            //            Assert.AreEqual(questionSummary.TotalQuestions, 4);
+//            //            Assert.AreEqual(questionSummary.CorrectQuestions, 2);
+//            //            break;
+
+//            //        }
+//            //    }
+
+//            //}
+
+//            //_thoughtList = new ListView();
+//            //_thoughtList.ItemsSource = thoughts;
+//            //_thoughtList.ItemTemplate = new DataTemplate(typeof(TextCell));
+//            //_thoughtList.ItemTemplate.SetBinding(TextCell.TextProperty, "Id");
+//            //_thoughtList.ItemTemplate.SetBinding(TextCell.DetailProperty, "Text");
+
+
+//            //Content = _thoughtList;
+//        }
+
+//        public void Refresh()
+//        {
+//           // _thoughtList.ItemsSource = _database.GetRandomQuestions(10);
+//        }
+//    }
+//}
