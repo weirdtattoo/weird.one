111b6aacdbff247a3770df583df2ed0220722749
kazi <kazi.develop@gmail.com>
2016-11-15 17:32:09 -0800

    adding UWP to project

---
+++ Weird/Weird.UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Weird.UWP
+{
+    public sealed partial class MainPage
+    {
+        private string dbPath = "";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Weird.App(dbPath));
+            
+        }
+    }
+}
